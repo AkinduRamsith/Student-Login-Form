1558ec474746523748ea59b785b60f87cb4b6254
AkinduRamsith <akiyaramsith2002@gmail.com>
2023-08-22 22:44:38 +0530

    Day 29

--- Back End/Student-Login-Form/src/main/java/edu/icet/dto/Student.java
+++ Back End/Student-Login-Form/src/main/java/edu/icet/dto/Student.java
@@ -18,46 +18,6 @@ public class Student {
     private String batch;
     private String gender;
 
-    public void setFirstName(String firstName){
-        if(null==firstName)return;
-        this.firstName=firstName;
-    }
-    public void setLastName(String lastName){
-        if(null==lastName)return;
-        this.lastName=lastName;
-    }
-    public void setAge(String age){
-        if(null==age && Integer.parseInt(age)<10)return;
-        this.age=age;
-    }
-    public void setPhoneNumber(String phoneNumber){
-        if(null==firstName && phoneNumber.length()!=10 && phoneNumber.substring(0,1).equals(0))return;
-        this.phoneNumber=phoneNumber;
-    }
 
-    public void setEmail(String email){
-        if(null==email)return;
-       this.email=email;
-    }
-
-    public void setAddress(String address){
-        if(null==address)return;
-        this.address=address;
-    }
-
-    public void setInstitute(String institute){
-        if(null==institute)return;
-        this.institute=institute;
-    }
-
-    public void setBatch(String batch){
-        if(null==batch)return;
-        this.batch=batch;
-    }
-
-    public void setGender(String gender){
-        if(null==gender)return;
-        this.gender=gender;
-    }
 
 }
--- Back End/Student-Login-Form/src/main/java/edu/icet/service/StudentServiceImplV1.java
+++ Back End/Student-Login-Form/src/main/java/edu/icet/service/StudentServiceImplV1.java
@@ -4,39 +4,45 @@ import edu.icet.dao.StudentEntity;
 import edu.icet.dto.Student;
 import edu.icet.repository.StudentRepository;
 
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class StudentServiceImplV1 implements StudentService{
+public class StudentServiceImplV1 implements StudentService {
     @Autowired
     StudentRepository studentRepository;
     @Autowired
     ModelMapper modelMapper;
+
     @Override
     public void saveStudent(Student student) {
 //        if(student == null)return;
-        StudentEntity studentEntity=new StudentEntity();
-        studentEntity.setFirstName(student.getFirstName());
-        studentEntity.setLastName(student.getLastName());
-        studentEntity.setAge(student.getAge());
-        studentEntity.setPhoneNumber(student.getPhoneNumber());
-        studentEntity.setEmail(student.getEmail());
-        studentEntity.setAddress(student.getAddress());
-        studentEntity.setInstitute(student.getInstitute());
-        studentEntity.setBatch(student.getBatch());
-        studentEntity.setGender(student.getGender());
-        studentRepository.save(studentEntity);
-
-//        StudentEntity map = modelMapper.map(student, StudentEntity.class);
-//        if(map.getAge()==null && map.getFirstName()==null){
+//        if ( student.getAge() == null && student.getAge().isBlank() && student.getFirstName() == null && student.getFirstName().isBlank()) {
+//
 //            return;
 //        }
-//
-//        studentRepository.save(map);
+//        StudentEntity studentEntity=new StudentEntity();
+//        studentEntity.setFirstName(student.getFirstName());
+//        studentEntity.setLastName(student.getLastName());
+//        studentEntity.setAge(student.getAge());
+//        studentEntity.setPhoneNumber(student.getPhoneNumber());
+//        studentEntity.setEmail(student.getEmail());
+//        studentEntity.setAddress(student.getAddress());
+//        studentEntity.setInstitute(student.getInstitute());
+//        studentEntity.setBatch(student.getBatch());
+//        studentEntity.setGender(student.getGender());
+//        studentRepository.save(studentEntity);
+
+        StudentEntity map = modelMapper.map(student, StudentEntity.class);
+        if (map.getAge() == null || map.getAge().isBlank() || map.getFirstName() == null || map.getFirstName().isBlank() || map.getPhoneNumber() == null || map.getPhoneNumber().isBlank() || map.getEmail() == null || map.getEmail().isBlank() || map.getAddress() == null || map.getAddress().isBlank() || map.getLastName() == null || map.getLastName().isBlank() || map.getBatch() == null || map.getBatch().isBlank() || map.getInstitute() == null || map.getInstitute().isBlank() || map.getGender()==null || map.getGender().isBlank()) {
+            System.out.println("Hello");
+            return;
+        }
+
+        studentRepository.save(map);
 
 
     }
